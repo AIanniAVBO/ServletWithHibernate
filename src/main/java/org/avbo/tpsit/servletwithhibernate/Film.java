021bb09b9c3f348e1832bea30ba678263a44851b
Andrea Iannì <andrea.ianni@avbo.it>
2024-05-08 15:42:24 +0200

    Implementata la classe per descrivere un film con le relative proprietà e costruttori (uno vuoto necessario per Hibernate)

--- src/main/java/org/avbo/tpsit/servletwithhibernate/Film.java
+++ src/main/java/org/avbo/tpsit/servletwithhibernate/Film.java
@@ -1,5 +1,53 @@
 package org.avbo.tpsit.servletwithhibernate;
 
-public class Film {
+import jakarta.persistence.*;
 
+@Entity
+@Table(name="film")
+public class Film {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="film_id", nullable=false)
+	private int filmID;
+    @Column(name="title", nullable=false)
+	private String title;
+    @Column(name="description")
+	private String description;
+    
+    public Film()
+    {
+    }
+    
+	public Film(String title, String description) {
+		super();
+		this.title = title;
+		this.description = description;
+	}
+
+	public Film(int filmID, String title, String description) {
+		super();
+		this.filmID = filmID;
+		this.title = title;
+		this.description = description;
+	}
+	
+	public synchronized int getFilmID() {
+		return filmID;
+	}
+	public synchronized void setFilmID(int filmID) {
+		this.filmID = filmID;
+	}
+	public synchronized String getTitle() {
+		return title;
+	}
+	public synchronized void setTitle(String title) {
+		this.title = title;
+	}
+	public synchronized String getDescription() {
+		return description;
+	}
+	public synchronized void setDescription(String description) {
+		this.description = description;
+	}
+    
 }
